19a6e7d0c291c947fb5a494fab13167b5816c1c2
Ryan Doyle <rbdoyle@wpi.edu>
2020-10-08 19:04:38 -0400

    No longer crashes when opening gameplay screen

--- app/src/main/java/com/example/connect4pro/GameActivity.kt
+++ app/src/main/java/com/example/connect4pro/GameActivity.kt
@@ -1,4 +1,31 @@
 package com.example.connect4pro
 
-class GameActivity {
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+private const val ID1 =
+    "private.name1"
+private const val ID2 =
+    "private.name2"
+
+private const val ID3 =
+    "private.name3"
+private const val ID4 =
+    "private.name4"
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, withAI: Boolean, playerColor: String): Intent {
+            return Intent(packageContext, GameActivity::class.java).apply {
+                putExtra(ID1, withAI)
+                putExtra(ID2, playerColor)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/connect4pro/MainActivity.kt
+++ app/src/main/java/com/example/connect4pro/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.connect4pro
 
+import android.content.ClipData.newIntent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var one_player: Button
+    private lateinit var two_player: Button
+    private lateinit var replays: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        one_player = findViewById(R.id.one_player)
+        two_player = findViewById(R.id.two_player)
+
+        one_player.setOnClickListener { view: View ->
+
+            val intent = GameActivity.newIntent(this@MainActivity, true, "red")
+            startActivity(intent)
+        }
+
+        two_player.setOnClickListener { view: View ->
+
+            val intent = GameActivity.newIntent(this@MainActivity, false, "red")
+            startActivityForResult(intent, 0)
+        }
     }
 }
