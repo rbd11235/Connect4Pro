0eb52b763fdfe6f3d6ac4c8e1643379379f37f6e
Ryan Doyle <rbdoyle@wpi.edu>
2020-10-09 00:14:25 -0400

    Ability to place pieces added. ImageViews in GameActivity no longer have individual variables but are instead stored in a two-dimensional array that matches with the one in the view model.

--- app/src/main/java/com/example/connect4pro/GameActivity.kt
+++ app/src/main/java/com/example/connect4pro/GameActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -22,53 +23,8 @@ class GameActivity : AppCompatActivity() {
     private lateinit var redLabel: TextView
     private lateinit var yellowLabel: TextView
 
-    private lateinit var A1: ImageView
-    private lateinit var A2: ImageView
-    private lateinit var A3: ImageView
-    private lateinit var A4: ImageView
-    private lateinit var A5: ImageView
-    private lateinit var A6: ImageView
-    private lateinit var A7: ImageView
-
-    private lateinit var B1: ImageView
-    private lateinit var B2: ImageView
-    private lateinit var B3: ImageView
-    private lateinit var B4: ImageView
-    private lateinit var B5: ImageView
-    private lateinit var B6: ImageView
-    private lateinit var B7: ImageView
-
-    private lateinit var C1: ImageView
-    private lateinit var C2: ImageView
-    private lateinit var C3: ImageView
-    private lateinit var C4: ImageView
-    private lateinit var C5: ImageView
-    private lateinit var C6: ImageView
-    private lateinit var C7: ImageView
-
-    private lateinit var D1: ImageView
-    private lateinit var D2: ImageView
-    private lateinit var D3: ImageView
-    private lateinit var D4: ImageView
-    private lateinit var D5: ImageView
-    private lateinit var D6: ImageView
-    private lateinit var D7: ImageView
-
-    private lateinit var E1: ImageView
-    private lateinit var E2: ImageView
-    private lateinit var E3: ImageView
-    private lateinit var E4: ImageView
-    private lateinit var E5: ImageView
-    private lateinit var E6: ImageView
-    private lateinit var E7: ImageView
-
-    private lateinit var F1: ImageView
-    private lateinit var F2: ImageView
-    private lateinit var F3: ImageView
-    private lateinit var F4: ImageView
-    private lateinit var F5: ImageView
-    private lateinit var F6: ImageView
-    private lateinit var F7: ImageView
+    //Two dimensional array containing all the ImageViews that make up the board
+    private lateinit var pieces: Array<Array<ImageView>>
 
     private lateinit var button1: ImageButton
     private lateinit var button2: ImageButton
@@ -77,6 +33,7 @@ class GameActivity : AppCompatActivity() {
     private lateinit var button5: ImageButton
     private lateinit var button6: ImageButton
     private lateinit var button7: ImageButton
+
 
     private val gameModel: GameModel by lazy {
         ViewModelProviders.of(this).get(GameModel::class.java)
@@ -95,90 +52,106 @@ class GameActivity : AppCompatActivity() {
         button6 = findViewById(R.id.drop6)
         button7 = findViewById(R.id.drop7)
 
-        A1 = findViewById(R.id.A1)
-        A2 = findViewById(R.id.A2)
-        A3 = findViewById(R.id.A3)
-        A4 = findViewById(R.id.A4)
-        A5 = findViewById(R.id.A5)
-        A6 = findViewById(R.id.A6)
-        A7 = findViewById(R.id.A7)
 
-        B1 = findViewById(R.id.B1)
-        B2 = findViewById(R.id.B2)
-        B3 = findViewById(R.id.B3)
-        B4 = findViewById(R.id.B4)
-        B5 = findViewById(R.id.B5)
-        B6 = findViewById(R.id.B6)
-        B7 = findViewById(R.id.B7)
+        val A1: ImageView = findViewById(R.id.A1)
+        val A2: ImageView = findViewById(R.id.A2)
+        val A3: ImageView = findViewById(R.id.A3)
+        val A4: ImageView = findViewById(R.id.A4)
+        val A5: ImageView = findViewById(R.id.A5)
+        val A6: ImageView = findViewById(R.id.A6)
+        val A7: ImageView = findViewById(R.id.A7)
+        val row1 = arrayOf(A1, A2, A3, A4, A5, A6, A7)
 
-        C1 = findViewById(R.id.C1)
-        C2 = findViewById(R.id.C2)
-        C3 = findViewById(R.id.C3)
-        C4 = findViewById(R.id.C4)
-        C5 = findViewById(R.id.C5)
-        C6 = findViewById(R.id.C6)
-        C7 = findViewById(R.id.C7)
 
-        D1 = findViewById(R.id.D1)
-        D2 = findViewById(R.id.D2)
-        D3 = findViewById(R.id.D3)
-        D4 = findViewById(R.id.D4)
-        D5 = findViewById(R.id.D5)
-        D6 = findViewById(R.id.D6)
-        D7 = findViewById(R.id.D7)
+        val B1: ImageView  = findViewById(R.id.B1)
+        val B2: ImageView = findViewById(R.id.B2)
+        val B3: ImageView = findViewById(R.id.B3)
+        val B4: ImageView  = findViewById(R.id.B4)
+        val B5: ImageView  = findViewById(R.id.B5)
+        val B6: ImageView  = findViewById(R.id.B6)
+        val B7: ImageView  = findViewById(R.id.B7)
 
-        E1 = findViewById(R.id.E1)
-        E2 = findViewById(R.id.E2)
-        E3 = findViewById(R.id.E3)
-        E4 = findViewById(R.id.E4)
-        E5 = findViewById(R.id.E5)
-        E6 = findViewById(R.id.E6)
-        E7 = findViewById(R.id.E7)
+        var row2 = arrayOf(B1, B2, B3, B4, B5, B6, B7)
 
-        F1 = findViewById(R.id.F1)
-        F2 = findViewById(R.id.F2)
-        F3 = findViewById(R.id.F3)
-        F4 = findViewById(R.id.F4)
-        F5 = findViewById(R.id.F5)
-        F6 = findViewById(R.id.F6)
-        F7 = findViewById(R.id.F7)
+        val C1: ImageView = findViewById(R.id.C1)
+        val C2: ImageView = findViewById(R.id.C2)
+        val C3: ImageView = findViewById(R.id.C3)
+        val C4: ImageView = findViewById(R.id.C4)
+        val C5: ImageView = findViewById(R.id.C5)
+        val C6: ImageView = findViewById(R.id.C6)
+        val C7: ImageView = findViewById(R.id.C7)
+
+        var row3 = arrayOf(C1, C2, C3, C4, C5, C6, C7)
+
+        val D1: ImageView = findViewById(R.id.D1)
+        val D2: ImageView = findViewById(R.id.D2)
+        val D3: ImageView = findViewById(R.id.D3)
+        val D4: ImageView = findViewById(R.id.D4)
+        val D5: ImageView = findViewById(R.id.D5)
+        val D6: ImageView = findViewById(R.id.D6)
+        val D7: ImageView = findViewById(R.id.D7)
+
+        var row4 = arrayOf(D1, D2, D3, D4, D5, D6, D7)
+
+        val E1: ImageView = findViewById(R.id.E1)
+        val E2: ImageView = findViewById(R.id.E2)
+        val E3: ImageView = findViewById(R.id.E3)
+        val E4: ImageView = findViewById(R.id.E4)
+        val E5: ImageView = findViewById(R.id.E5)
+        val E6: ImageView = findViewById(R.id.E6)
+        val E7: ImageView = findViewById(R.id.E7)
+
+        var row5 = arrayOf(E1, E2, E3, E4, E5, E6, E7)
+
+        val F1: ImageView = findViewById(R.id.F1)
+        val F2: ImageView = findViewById(R.id.F2)
+        val F3: ImageView = findViewById(R.id.F3)
+        val F4: ImageView = findViewById(R.id.F4)
+        val F5: ImageView = findViewById(R.id.F5)
+        val F6: ImageView = findViewById(R.id.F6)
+        val F7: ImageView = findViewById(R.id.F7)
+
+        var row6 = arrayOf(F1, F2, F3, F4, F5, F6, F7)
+
+        //Fills up the pieces array
+        pieces = arrayOf(row1, row2, row3, row4, row5, row6)
 
         gameModel.withAI = intent.getBooleanExtra(ID1, false)
         gameModel.playerColor = intent.getIntExtra(ID2, 1)
         //Red always starts the game
         gameModel.whoseTurn = RED
         button1.setOnClickListener { view: View ->
-
+           takeTurn(gameModel.whoseTurn, 1)
 
         }
 
         button2.setOnClickListener { view: View ->
-
+            takeTurn(gameModel.whoseTurn, 2)
 
         }
 
         button3.setOnClickListener { view: View ->
-
+            takeTurn(gameModel.whoseTurn, 3)
 
         }
 
         button4.setOnClickListener { view: View ->
-
+            takeTurn(gameModel.whoseTurn, 4)
 
         }
 
         button5.setOnClickListener { view: View ->
-
+            takeTurn(gameModel.whoseTurn, 5)
 
         }
 
         button6.setOnClickListener { view: View ->
-
+            takeTurn(gameModel.whoseTurn, 6)
 
         }
 
         button7.setOnClickListener { view: View ->
-
+            takeTurn(gameModel.whoseTurn, 7)
 
         }
     }
@@ -186,6 +159,49 @@ class GameActivity : AppCompatActivity() {
     private fun takeTurn(color: Int, slot: Int)
     {
 
+        placePiece(color, slot)
+
+        //Check if game over
+
+            //End game setup
+
+        //
+    }
+
+    private fun placePiece(color: Int, slot: Int)
+    {
+        var row = -1
+
+        for(x in 5 downTo(0))
+        {
+            if(gameModel.gameBoard[x][slot-1] == 0)
+            {
+                row = x
+            }
+        }
+        if(row == -1)
+        {
+            Log.d("Errors", "No empty spaces in column ")
+        }
+        else
+        {
+            gameModel.gameBoard[row][slot-1] = color
+            anim(color, row, slot-1)
+        }
+    }
+
+    //Updates the image views to place the piece on the board
+    //If we have time, we can add more complex "animation"
+    private fun anim(color: Int, row: Int, col: Int)
+    {
+        if(color == 1)
+        {
+            pieces[row][col].setImageResource(R.drawable.redpiece)
+        }
+        else if(color == 2)
+        {
+            pieces[row][col].setImageResource(R.drawable.yellowpiece)
+        }
     }
     companion object {
         fun newIntent(packageContext: Context, withAI: Boolean, playerColor: Int): Intent {
